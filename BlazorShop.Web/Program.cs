aa6bb6f1962751dfed5842fcd8bbb9b57e7fa8b7
Humberto Marcone <humberto.freitas@ionsistemas.com.br>
2022-09-20 11:16:14 -0300

    Blazor WebAssembly - Criando uma loja virtual - III

--- BlazorShop.Web/Program.cs
+++ BlazorShop.Web/Program.cs
@@ -1,4 +1,5 @@
 using BlazorShop.Web;
+using BlazorShop.Web.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,13 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var baseUrl = "https://localhost:7209";
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(baseUrl)
+});
+
+builder.Services.AddScoped<IProdutoService, ProdutoService>();
 
 await builder.Build().RunAsync();
